c19de74778f3988c19d459932b1d9a64fff3205e
Gean <gean.marroquin@gmail.com>
2019-09-07 22:31:09 -0500

    fix cors

--- src/lambda/hello-world.js
+++ src/lambda/hello-world.js
@@ -46,6 +46,9 @@ exports.handler = (event, context, callback) => {
       });
     callback(null, {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify({
         data: answers
       })
